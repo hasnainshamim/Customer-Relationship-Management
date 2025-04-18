c93cc6592b80f8210ba880f972e9db26dc2c01dd
hussain shamim <hussainshamimdev@gmail.com>
2025-04-19 02:46:58 +0500

    live kar rha hon Crm

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: '/', // Ensure this is set correctly
+  build: {
+    outDir: 'dist', // This is default for Vite
+  }
 })
